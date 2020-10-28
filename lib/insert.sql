f19e5e29a8dbd0035433da43986714b6dc7bce6e
apontigo <PontigoAndres@gmail.com>
2020-10-27 23:00:59 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+insert into bears (id,name,age,gender,color,temperament,alive) values (1,'Mr. Chocolate',10,'M','Brown','Mild',1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (2,'Rowdy',6,'M','Black','Hot',1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (3,'Tabitha',8, 'F','Black','Cold',0)
+insert into bears (id,name,age,gender,color,temperament,alive) values (4,'Sergeant Brown',20,'M','Brown','Hot',1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (5,'Melissa',4,'F','Brown','Cold',1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (6,'Grinch',9,'M','Brown','Hot',0);
+insert into bears (id,name,age,gender,color,temperament,alive) values (7,'Wendy',1,'F','Black','Mild',1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (8,NULL,19,'M','Brown','Hot',0);
+
